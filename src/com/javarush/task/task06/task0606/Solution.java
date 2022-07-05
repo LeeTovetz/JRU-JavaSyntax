3548558da053f07fb098c5a17aaddf869d48e0c8
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2022-07-05 16:23:53 +0300

    task0606
    task0606

---
+++ src/com/javarush/task/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+    }
+
+    public static void universalMethod(String name) {
+    }
+
+    public static void universalMethod(String name, int age) {
+    }
+
+    public static void universalMethod(int age) {
+    }
+
+    public static void universalMethod(int age, int weight) {
+    }
+
+    public static void universalMethod(double weight, double growth) {
+    }
+
+    public static void universalMethod(double growth) {
+    }
+
+    public static void universalMethod(String name, double weight) {
+    }
+
+    public static void universalMethod(char a, char b) {
+    }
+
+    public static void universalMethod(boolean bool) {
+    }
+
+    //напишите тут ваш код
+}
