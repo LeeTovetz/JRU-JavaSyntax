71be76f94db7bfa14f80298ccab5b06ca5604754
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2022-07-13 20:04:14 +0300

    task0701
    task0701

---
+++ src/com/javarush/task/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte a = 1;
+    short b = 2;
+    int c = 3;
+    long d = 4L;
+    float f = 5f;
+    double db = 52.0;
+    boolean bl = false;
+    char ch = 'a';
+}
