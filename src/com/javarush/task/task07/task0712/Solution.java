0a40502c32bc81eac644e7c0d83698357d913f0b
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2022-07-14 18:29:33 +0300

    task0712
    task0712

---
+++ src/com/javarush/task/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s1 = new String("");
+        String s2 = new String("");
+        String s3 = new String("");
+        String s4 = new String("");
+        String s5 = new String("");
+        String s6 = new String("");
+        String s7 = new String("");
+        String s8 = new String("");
+        String s9 = new String("");
+        String s10 = new String("");
+
+        int[] ints1 = new int[10];
+        int[] ints2 = new int[10];
+        int[] ints3 = new int[10];
+        int[] ints4 = new int[10];
+        int[] ints5 = new int[10];
+
+        Scanner sc1 = new Scanner("");
+        Scanner sc2 = new Scanner("");
+    }
+}
