bcf66eda5b69e5b8503e2836956fd52451e93ca1
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2022-07-15 00:28:33 +0300

    task0714
    task0714

---
+++ src/com/javarush/task/task07/task0714/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task07.task0714;
+
+public class Solution {
+    public static void main(String[] args) {
+        Car car = new Car();
+        ElectricCar electricCar = new ElectricCar();
+        Vehicle vehicle = new Vehicle();
+    }
+}
